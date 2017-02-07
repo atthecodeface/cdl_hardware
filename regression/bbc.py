b1aa66dda7632d711c5ed51ddab8f52095386d5e
Gavin Stark <gjstark@me.com>
2017-02-07 10:55:26 +0000

    [bbc] Fixed CRTC writes to use cpu_data_out not main_databus, to fully put in the 'timing' fix

--- regression/bbc.py
+++ regression/bbc.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 #a Copyright
-#  
+#
 #  This file 'bbc.py' copyright Gavin J Stark 2016, 2017
-#  
+#
 #  This program is free software; you can redistribute it and/or modify it under
 #  the terms of the GNU General Public License as published by the Free Software
 #  Foundation, version 2.0.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even implied warranty of MERCHANTABILITY
 #  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
@@ -48,7 +48,7 @@ class c_TestBBC_HW(SimulationTestBase):
         # dec 11 lunchtime - 100 million step takes 40 seconds, debug build; 25 seconds optimized build
         # each 10 steps is one system clock = 4MHz = 250ns => 25ns/step
         # -> 40Msteps / bbc second => 100 million steps is 2.5 bbc seconds => 40/2.5 = runs at 1/16th realtime debug
-        # -> 40Msteps / bbc second => 100 million steps is 2.5 bbc seconds => 25/2.5 = runs at 1/10th realtime 
+        # -> 40Msteps / bbc second => 100 million steps is 2.5 bbc seconds => 25/2.5 = runs at 1/10th realtime
         # without getting screen every 100k steps saves 8% performance
         #hw.step(72600000)
         while not do_waves:
@@ -58,10 +58,10 @@ class c_TestBBC_HW(SimulationTestBase):
             hw.th.display_screen()
             pass
         if do_waves:
-            #hw.th.save_screen("fred")
+            hw.step(240000000)
             print "Waves enabled"
             waves.enable()
-            hw.step(1*1000*1000)
+            hw.step(3*1000*1000)
             print "Waves stopped"
             pass
         pass
