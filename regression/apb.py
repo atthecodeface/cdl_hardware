dafcb7a01168e5a9b1d06f3e7b8e83b35bb0e7ba
Gavin Stark <gjstark@me.com>
2017-02-18 15:25:24 +0000

    [apb] Refactored APB rom compilation

--- regression/apb.py
+++ regression/apb.py
@@ -15,73 +15,13 @@
 #a Imports
 import pycdl
 import sys, os, unittest
+import apb_rom
 import simple_tb
 
 #a Test classes
 #c c_rom_th
-class c_rom_th(simple_tb.base_th):
-    opcodes = {
-        "opcode_class_alu": 0,
-        "opcode_class_set_parameter": 1,
-        "opcode_class_apb_request": 2,
-        "opcode_class_branch": 3,
-        "opcode_class_wait": 4,
-        "opcode_class_finish": 5
-        }
-
-    opcode_subclass = {
-        "rom_op_alu_or": 0,
-        "rom_op_alu_and": 1,
-        "rom_op_alu_bic": 2,
-        "rom_op_alu_xor": 3,
-        "rom_op_alu_add": 4,
-
-        "rom_op_set_address": 0,
-        "rom_op_set_repeat": 1,
-        "rom_op_set_accumulator": 2,
-
-        "rom_op_branch": 0,
-        "rom_op_beq": 1,
-        "rom_op_bne": 2,
-        "rom_op_loop": 3,
-
-        "rom_op_req_read": 0,
-        "rom_op_req_write_arg": 1,
-        "rom_op_req_write_acc": 2,
-        "rom_op_req_read_inc": 4, # auto-increment
-        "rom_op_req_write_arg_inc": 5,
-        "rom_op_req_write_acc_inc": 6,
-        }
-    #f op_alu
-    @classmethod
-    def op_alu(cls, alu_op, data):
-        return (cls.opcodes["opcode_class_alu"]<<(32+5)) | (cls.opcode_subclass["rom_op_alu_"+alu_op]<<32) | data
-        pass
-    #f op_branch
-    @classmethod
-    def op_branch(cls, branch_op, data):
-        return (cls.opcodes["opcode_class_branch"]<<(32+5)) | (cls.opcode_subclass["rom_op_"+branch_op]<<32) | data
-        pass
-    #f op_set
-    @classmethod
-    def op_set(cls, set_op, data):
-        return (cls.opcodes["opcode_class_set_parameter"]<<(32+5)) | (cls.opcode_subclass["rom_op_set_"+set_op]<<32) | data
-        pass
-    #f op_req
-    @classmethod
-    def op_req(cls, req_op, data):
-        return (cls.opcodes["opcode_class_apb_request"]<<(32+5)) | (cls.opcode_subclass["rom_op_req_"+req_op]<<32) | data
-        pass
-    #f op_wait
-    @classmethod
-    def op_wait(cls, data):
-        return (cls.opcodes["opcode_class_wait"]<<(32+5)) | data
-        pass
-    #f op_finish
-    @classmethod
-    def op_finish(cls):
-        return (cls.opcodes["opcode_class_finish"]<<(32+5))
-        pass
+class c_rom_th(simple_tb.base_th, apb_rom.rom):
+    pass
 
 #c c_test_one
 class c_test_one(c_rom_th):
@@ -128,34 +68,13 @@ class c_test_one(c_rom_th):
     expectation = [7, 6, 7, 6, 7, 6, 7, 6, 4, 0, 7, 6, 4, 0] + [7, 6, 4, 0] + [7, 6, 4, 0]
     #f fill_rom
     def fill_rom(self, program, address=0):
-        label_addresses = {}
-        for p in range(2):
-            prog_address = address
-            for op_labels in program["code"]:
-                if len(op_labels)==1:
-                    op=op_labels[0]
-                    labels=[]
-                    pass
-                else:
-                    (op,labels) = op_labels
-                    pass
-                if p==0:
-                    for l in labels:
-                        if l[-1]==':': label_addresses[l] = prog_address
-                        pass
-                    pass
-                else:
-                    for l in labels:
-                        if l[-1]!=':': op |= label_addresses[l+":"]
-                        pass
-                    self.sim_msg.send_value("dut.apb_rom",9,0,prog_address,op)
-                    pass
-                prog_address = prog_address + 1
-                pass
+        compilation = apb_rom.rom.compile_program(program,address)
+        for (prog_address, op) in compilation["object"]:
+            self.sim_msg.send_value("dut.apb_rom",9,0,prog_address,op)
             pass
         entry_addresses = []
         for ep in program["entry_points"]:
-            entry_addresses.append(label_addresses[ep+":"])
+            entry_addresses.append(compilation["labels"][ep+":"])
             pass
         return entry_addresses
     #f bfm_tick
