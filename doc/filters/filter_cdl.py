9759c4400f5f099e47c9561b3074342e7fd9dacd
Gavin Stark <gjstark@me.com>
2017-03-03 16:18:36 +0000

    [documentation] Much improvement of the filter

--- doc/filters/filter_cdl.py
+++ doc/filters/filter_cdl.py
@@ -3,6 +3,40 @@
 import sys, re, unittest
 
 #a Classes
+#c c_fsm
+class c_fsm:
+    def __init__(self, cdl_file):
+        self.name          = "fsm%d"%cdl_file.get_uid()
+        self.dot_filename  = "dots/%s__fsm__%s.dot"%(cdl_file.filename_leaf_root,self.name)
+        self.current_state   = "no known state"
+        self.states = []
+        self.transitions = []
+        pass
+    def add_state(self, state):
+        self.states.append(state)
+        pass
+    def add_transition(self, src=None, tgt=None):
+        if src is None: src=self.states[-1]
+        if tgt is None: src=self.states[-1]
+        self.transitions.append((src,tgt))
+        pass
+    def header(self):
+        return "/** @dotfile %s **/"%self.dot_filename
+    def write_dotfile(self):
+        dot_file = open(self.dot_filename, "w")
+        print >> dot_file, "digraph fsm__%s {"%self.name
+        print >> dot_file, " node [shape=record, fontname=Helvetica, fontsize=10];  "
+        for s in self.states:
+            print >> dot_file, ' %s [ label="%s" ]; '%(s,s)
+            pass
+        for (s,t) in self.transitions:
+            print >> dot_file, ' %s -> %s [ arrowhead="open", style="dashed" ]; '%(s,t)
+            pass
+        print >> dot_file, '}'
+        dot_file.close()
+        pass
+
+
 #c c_cdl_file
 class c_cdl_file:
     @staticmethod
@@ -25,34 +59,41 @@ class c_cdl_file:
         self.is_header_file = (self.filename_ext=="h")
         self.is_source_file = (self.filename_ext=="cdl")
         pass
+    #f get_uid
+    def get_uid(self):
+        self.uid = self.uid + 1
+        return self.uid
+    #f enter_module
+    def enter_module(self):
+        """Source files use a group to pull the module documentation together; header files are global"""
+        if self.is_source_file:
+            return "namespace %s {/** @addtogroup %s %s */ /**@{ */"%(self.filename_leaf_root,
+                                                                             self.filename_leaf_root,
+                                                                             self.filename_leaf_root)
+        return ""
+    #f leave_module
+    def leave_module(self):
+        """Leaving a module - for example before an include - permits namespace separation (kinda)"""
+        if self.is_source_file:
+            return " /** @} */ }"
+            pass
+        return ""
     #f header
     def header(self):
         """
         Return any header line for the file, without newlines (to not effect line numbers)
-
-        For source files, output a namespace so that the module in the
-        source file is a namespace - and any local structures etc are
-        deemed local to that module, as desired.
-
-        Header files in CDL are part of the global namespace, so do
-        not 'namespace' them.
         """
-        if self.is_source_file:
-            return r"/** \addtogroup %s **/namespace %s {"%(self.filename_leaf_root, self.filename_leaf_root)
-            pass
-        return ""
+        r = ""
+        r += self.enter_module()
+        return r
     #f footer
     def footer(self):
         """
         Output any footer line for the file, without a newline at the
         end (to not effect line numbers)
-
-        Source files have to close the module's 'namespace'
         """
-        if self.is_source_file:
-            return "}"
-            pass
-        return ""
+        r = self.leave_module()
+        return r
     #f find_first
     def find_first(self, l, r, strings):
         """
@@ -76,10 +117,20 @@ class c_cdl_file:
         pend = min_pos[0]+len(min_pos[1])
         r = r + l[:pend]
         return (l[pend:], r, min_pos[1])
+    #f try_parse_user_id
+    def try_parse_user_id(self, l):
+        """
+        Try to find a user id at the start of the string, possibly
+        after whitespace. Return None if none.
+        """
+        m=re.match("(\s*)([a-zA-Z_][a-zA-Z_0-9]*)(.*)",l)
+        if m is None: return (None,None,l)
+        return (m.group(2),m.group(1),m.group(3)+"\n")
     #f parse_init
     def parse_init(self):
         self.comment_start_strings = ["/*", "//", '"""', '"']
         self.pending_documentation = ""
+        self.uid = 0
         self.parse_states = ["head"]
         self.parse_fns = {"head":self.parse_head,
                           "include":self.parse_include,
@@ -87,21 +138,27 @@ class c_cdl_file:
                           "typedef":self.parse_typedef,
                           "typedef_fsm":self.parse_typedef_fsm,
                           "typedef_fsm_body":self.parse_typedef_fsm_body,
+                          "typedef_fsm_transitions":self.parse_typedef_fsm_transitions,
                           "singleline_doc":self.parse_singleline_doc,
                           "multiline_doc":self.parse_multiline_doc,
                           "module_header":self.parse_module_header,
+                          "module_ports":self.parse_module_ports,
+                          "module_port_declaration":self.parse_module_port_declaration,
                           "module_body":self.parse_module_body,
+                          "code_block":self.parse_code_block,
+                          "code_block_brace":self.parse_code_block,
                           "signal_declaration":self.parse_signal_declaration,
                           "finish_statement":self.parse_finish_statement,
                           }
         pass
     #f parse_line
     def parse_line(self, l, result_so_far=""):
+        self.post_newline = ""
         while len(l)>0:
             (l,result_so_far,states) = self.parse_fns[self.parse_states[-1]](l, result_so_far)
             if states is not None: self.parse_states=states
             pass
-        return result_so_far
+        return result_so_far+self.post_newline
     #f parse_comment
     def parse_comment(self, l, r):
         (nl, nr, which) = self.find_first(l, r, ["*/"])
@@ -123,19 +180,13 @@ class c_cdl_file:
         return (nl, nr[:-1]+'*/', None)
     #f parse_include
     def parse_include(self, l, r):
-        if self.is_source_file:
-            r += "}"
-            pass
+        r += self.leave_module()
         r += '#include'
-        p = l.find('"')
-        p = p+1+l[p+1:].find('"')
-        r += l[:p+1]
-        l = l[p+1:]
         if self.is_source_file:
-            r += " namespace %s {"%(self.filename_leaf_root)
+            self.post_newline += self.enter_module()
             pass
         self.parse_states.pop()
-        return (l, r, None)
+        return ("", r+l, None)
     #f parse_typedef
     def parse_typedef(self, l, r):
         enum = re.match(r'(\s*enum)(\s*\[.*\])(.*$)',l)
@@ -147,6 +198,11 @@ class c_cdl_file:
             return (l, r, None)
             pass
         if fsm is not None:
+            self.fsm = c_fsm(self)
+            r = r[:-7]+self.fsm.header()+"typedef"
+            #r += fsm.group(1)+self.fsm.header()+"enum" # leads to anon enum
+            #r += fsm.group(1)+"enum" + self.fsm.header()
+            #r += self.fsm.header()+fsm.group(1)+"enum" # leads to anon enum
             r += fsm.group(1)+"enum"
             l = fsm.group(2)+"\n"
             self.parse_states[-1] = "typedef_fsm"
@@ -156,6 +212,7 @@ class c_cdl_file:
         return (l, r, None)
     #f parse_typedef_fsm
     def parse_typedef_fsm(self, l, r):
+        """Parse everything post 'typedef fsm'"""
         (nl, nr, which) = self.find_first(l, r, ["{", ] + self.comment_start_strings )
         (nl, nr, which, parsed) = self.parse_comment_start_string( nl, nr, which )
         if parsed: return (nl, nr, which)
@@ -165,14 +222,49 @@ class c_cdl_file:
         return ("", r+l, None)
     #f parse_typedef_fsm_body
     def parse_typedef_fsm_body(self, l, r):
+        """Parse everything post 'typedef fsm {'"""
+        (u,ws,nl) = self.try_parse_user_id(l)
+        if u is not None:
+            self.fsm.add_state(u)
+            pass
         (nl, nr, which) = self.find_first(l, r, [";", "{", "}", ] + self.comment_start_strings )
         (nl, nr, which, parsed) = self.parse_comment_start_string( nl, nr, which )
         if parsed: return (nl, nr, which)
+        if which == "{":
+            return (nl, nr[:-1], self.parse_states + ["typedef_fsm_transitions"])
         if which == "}":
+            #nr = nr[:-1]
+            #nr+=",state_diagram "+self.fsm.header()
+            #nr += "}"
+            #nr += self.fsm.header() # Leads to no dot file
+            if False: # leads to no dot file
+                (u,ws,nnl) = self.try_parse_user_id(nl)
+                if u is not None:
+                    nr += ws + u + self.fsm.header()
+                    nl = nnl
+                    pass
+                pass
+            self.fsm.write_dotfile()
             self.parse_states.pop()
             return (nl, nr, None)
         if which == ";":
             return (nl, nr[:-1]+",", None)
+        return ("", r+l, None)
+    #f parse_typedef_fsm_transitions
+    def parse_typedef_fsm_transitions(self, l, r):
+        """Parse a list of transition targets"""
+        (u,ws,nl) = self.try_parse_user_id(l)
+        if u is not None:
+            self.fsm.add_transition(tgt=u)
+            l = nl
+        (nl, nr, which) = self.find_first(l, r, ["}", "," ] + self.comment_start_strings )
+        (nl, nr, which, parsed) = self.parse_comment_start_string( nl, nr, which )
+        if parsed: return (nl, nr, which)
+        if which == "}":
+            self.parse_states.pop()
+            return (nl, nr[:-1], None)
+        if which == ",":
+            return (nl, nr[:-1], None)
         return ("", r+l, None)
     #f parse_comment_start_string
     def parse_comment_start_string(self, nl, nr, which):
@@ -198,19 +290,69 @@ class c_cdl_file:
         return ("", r+l, None)
     #f parse_module_header
     def parse_module_header(self, l, r):
-        (nl, nr, which) = self.find_first(l, r, ["{"]  + self.comment_start_strings )
-        if self.verbose:print ">>>>>%s___%s___%s___%s___"%(l,nl,nr,which)
+        (nl, nr, which) = self.find_first(l, r, ["(", "{"]  + self.comment_start_strings )
         (nl, nr, which, parsed) = self.parse_comment_start_string( nl, nr, which )
         if parsed: 
             return (nl, nr, which)
-        if which == "{": return (nl, nr, ["module_body"])
+        if which == "(":
+            return (nl, nr, self.parse_states+["module_ports"])
+        if which == "{":
+            return (nl, nr, ["module_body"])
+        return ("", r+l, None)
+    #f parse_module_ports
+    def parse_module_ports(self, l, r):
+        (nl, nr, which) = self.find_first(l, r, ["clock", "input", "output", ")"]  + self.comment_start_strings )
+        (nl, nr, which, parsed) = self.parse_comment_start_string( nl, nr, which )
+        if parsed: 
+            return (nl, nr, which)
+        if which in ["clock", "input", "output"]:
+            return (nl+which, nr, self.parse_states+["module_port_declaration"])
+        if which == ")":
+            self.parse_states.pop()
+            return (nl, nr, None)
+        return ("", r+l, None)
+    #f parse_module_port_declaration
+    def parse_module_port_declaration(self, l, r):
+        which = None # in case of multiline declaration
+        if l[-5:]=="clock": which="clock"
+        if l[-5:]=="input": which="input"
+        if l[-6:]=="output": which="output"
+        if which is not None:
+            l = l[:-len(which)]
+            if which in ["input", "output"]:
+                (u,ws,nl) = self.try_parse_user_id(l)
+                if u is not None:
+                    r += ws+u
+                    l = nl
+                    m = re.match("(\s*\[[^\]]*\])(.*)", l)
+                    if m:
+                        r += m.group(1)
+                        l = m.group(2)+"\n"
+                        pass
+                    pass
+                pass
+            (u,ws,nl) = self.try_parse_user_id(l)
+            if u is not None:
+                self.pending_documentation = '@param %s%s '%({"clock":"[in]","input":"[in]","output":"[out]"}[which],u)
+                pass
+            pass
+        (nl, nr, which) = self.find_first(l, r, [",", ")"]  + self.comment_start_strings )
+        (nl, nr, which, parsed) = self.parse_comment_start_string( nl, nr, which )
+        if parsed: 
+            return (nl, nr, which)
+        if which in [","]:
+            self.parse_states.pop()
+            return (nl, nr, None)
+        if which == ")":
+            self.parse_states.pop()
+            return (nl+")", nr[:-1], None)
         return ("", r+l, None)
     #f parse_module_body
     def parse_module_body(self, l, r):
         self.pending_documentation = ""
         code_block_match = re.match(r'\s*([^ ]*)\s*"""',l)
         if code_block_match:
-            self.pending_documentation='\section %s'%code_block_match.group(1)
+            self.pending_documentation="@section %s__%s"%(self.filename_leaf_root,code_block_match.group(1))
             pass
         (nl, nr, which) = self.find_first(l, r, ["clocked", "comb", "net", ":"] + self.comment_start_strings )
         (nl, nr, which, parsed) = self.parse_comment_start_string( nl, nr, which )
@@ -218,6 +360,27 @@ class c_cdl_file:
             return (nl, nr, which)
         if which in ["clocked", "comb", "net"]:
             return (which+nl, nr, self.parse_states+["signal_declaration"])
+        if which in [":"]:
+            return (nl, nr, self.parse_states+["code_block"])
+        return ("", r+l, None)
+    #f parse_code_block
+    def parse_code_block(self, l, r):
+        (nl, nr, which) = self.find_first(l, r, ["{", "}"] + self.comment_start_strings )
+        (nl, nr, which, parsed) = self.parse_comment_start_string( nl, nr, which )
+        if parsed: 
+            return (nl, nr, which)
+        if which in ["{"]:
+            return (nl, nr, self.parse_states+["code_block_brace"])
+        if which in ["}"]:
+            if self.parse_states[-1] != "code_block_brace":
+                raise Exception("Mismatch in braces")
+            self.parse_states.pop()
+            if self.parse_states[-1] == "code_block_brace":
+                return (nl, nr, None)
+            if self.parse_states[-1] != "code_block":
+                raise Exception("Mismatch in braces - expected end of code block")
+            self.parse_states.pop()
+            return (nl, nr, None)
         return ("", r+l, None)
     #f parse_finish_statement
     def parse_finish_statement(self, l, r):
@@ -225,8 +388,10 @@ class c_cdl_file:
         (nl, nr, which, parsed) = self.parse_comment_start_string( nl, nr, which )
         if parsed: 
             return (nl, nr, which)
-        self.parse_states.pop()
-        return (l, r, None)
+        if which==";":
+            self.parse_states.pop()
+            return (nl, nr, None)
+        return (nl, nr, None)
     #f parse_signal_declaration
     def parse_signal_declaration(self, l, r):
         if   l[:3] == "net":     which="net"
@@ -248,11 +413,13 @@ class c_cdl_file:
         if signame is None:
             self.parse_states.pop()
             return ("", r+l, None)
-        self.pending_documentation = 'param %s:%s '%(sigtype,signame)
+        self.pending_documentation = '@param %s:%s '%(sigtype,signame)
         r += l[:p]
         l = l[p:]
-        if not has_documentation:
-            l = '""' + l
+        # Do the following to 'insert' documentation - probably unwise, and better to encourage documentation in the code
+        # It also breaks if reset values spread across a line and documentation is therefore on the next line
+        #if not has_documentation:
+        #    l = '""' + l
         self.parse_states.pop()
         return (l, r, self.parse_states+["finish_statement"])
     #f All done
@@ -306,8 +473,8 @@ class c_test_include(c_base_test):
     input_text = r"""
     include "fred.h" """
     output_text = r"""/** \addtogroup a **/namespace a {
-    }#include "fred.h" namespace a { 
-}"""
+    }#include "fred.h" 
+namespace a {}"""
     test0 = c_base_test.base_test
     pass
 
@@ -366,9 +533,9 @@ class c_test_singleline_document(c_base_test):
     Continues """
     done'''
     output_text = r"""
-    This is test /**text*/ /
-    /** Stuff in comments
-    Continues *//***/
+    This is test /** text*/ /
+    /**  Stuff in comments
+    Continues *//** */
     done
 """
     test0 = c_base_test.base_test
@@ -383,8 +550,8 @@ class c_test_multiline_document(c_base_test):
     Continues """
     done'''
     output_text = r"""
-    This is test /**text*/ /
-    /** Stuff in comments
+    This is test /** text*/ /
+    /**  Stuff in comments
     Continues */
     done
 """
@@ -406,11 +573,11 @@ class c_test_multiline_document_comments(c_base_test):
     output_text = r'''
     // /* """
     /* """ */
-    This is test /**text*/ /
-    /** Stuff in comments
+    This is test /** text*/ /
+    /**  Stuff in comments
     Continues */
-    /** // fred */
-    /** /* fred */ */
+    /**  // fred */
+    /**  /* fred */ */
     done
 '''
     test0 = c_base_test.base_test
@@ -435,6 +602,23 @@ class c_test_fsm_simple(c_base_test):
     input_text = r"""
     typedef fsm {
     joe;
+    jim;
+    } fred;"""
+    output_text = r"""
+    typedef enum {
+    joe,
+    jim,
+    } fred;
+"""
+    test0 = c_base_test.base_test
+    pass
+
+#c c_test_fsm_transitions
+class c_test_fsm_transitions(c_base_test):
+    filename = 'a.h'
+    input_text = r"""
+    typedef fsm {
+    joe {peter, bunny};
     jim;
     } fred;"""
     output_text = r"""
@@ -494,7 +678,7 @@ class c_test_module_code_block(c_base_test):
     }'''
     output_text = r"""
     module fred() {
-    code_block /** \section code_block
+    code_block /** @section a__code_block
     My documented code block
     */ : {
     }
@@ -519,11 +703,11 @@ The module combinatorially takes in a hex value, and drives out 7 LED
 values.
 """{}'''
     output_text = r"""
-module led_seven_segment( input bit[4] hex   /**Hexadecimal to display on 7-segment LED*/,
-                          output bit[7] leds /**1 for LED on, 0 for LED off, for segments a-g in bits 0-7*/
+module led_seven_segment( input bit[4] hex   /** Hexadecimal to display on 7-segment LED*/,
+                          output bit[7] leds /** 1 for LED on, 0 for LED off, for segments a-g in bits 0-7*/
     )
     /*b Documentation */
-/**
+/** 
 Simple module to map a hex value to the LEDs required to make the
 appropriate symbol in a 7-segment display.
 
