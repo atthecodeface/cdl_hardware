6d3b95691fdb74a63dc3104fff7549d9fccda0b7
Gavin Stark <gjstark@me.com>
2018-05-22 05:52:11 -0700

    [multdiv] Python passing regression, cdl coding ongoing - python needs one cycle removed from division

--- python/multiplier.py
+++ python/multiplier.py
@@ -26,176 +26,6 @@ b_reg_hold = 0
 b_reg_b_in = 1
 b_reg_abs_b_in = 2
 
-#c multdiv_old
-class multdiv:
-    #f __init__
-    def __init__(self):
-        self.accumulator = 0 # 64 bits
-        self.multiplier = 0 # 32 bits
-        self.multiplicand = 0 # 32 bits
-        self.stage = 0 # 4 bits (maybe 5)
-        self.a_reg = 0
-        self.b_reg = 0
-    #f comb_new
-    def comb_new(self, a_in, b_in, mult4, shf, ah_0, ah_1, al_1, set_bit, bit_to_set, carry_chain, ar, br):
-        sel0123 = mult4 & 3
-        sel048c = (mult4>>2) & 3
-        stage1_0 = 0
-        stage1_1 = self.a_reg
-        stage1_2 = self.a_reg<<1
-        stage1_3 = stage1_2 + stage1_1 # 34-bit ADDER
-        stage1_4 = stage1_1 << 2
-        stage1_8 = stage1_2 << 2
-        stage1_c = stage1_3 << 2
-        if sel0123==0: mux_0123 = stage1_0
-        if sel0123==1: mux_0123 = stage1_1
-        if sel0123==2: mux_0123 = stage1_2
-        if sel0123==3: mux_0123 = stage1_3
-        if sel048c==0: mux_048c = stage1_0
-        if sel048c==1: mux_048c = stage1_4
-        if sel048c==2: mux_048c = stage1_8
-        if sel048c==3: mux_048c = stage1_c
-        mult_result = mux_0123 + mux_048c # 36-bit adder
-
-        shf_hl = mult_result << (4*shf)
-        shf_l = shf_hl & mask32
-        shf_h = (shf_hl>>32) & mask32
-
-        acc_l = self.accumulator & mask32
-        acc_h = (self.accumulator>>32) & mask32
-
-        a_in = a_in & mask32
-        neg_a_in = (-a_in) & mask32
-        abs_a_in = a_in
-        if (a_in & sign_bit): abs_a_in = neg_a_in
-
-        b_in = b_in & mask32
-        neg_b_in = (-b_in) & mask32
-        abs_b_in = b_in
-        if (b_in & sign_bit): abs_b_in = neg_b_in
-
-        alu_h_in_0 = 0
-        if ah_0 == ah_0_acc:      alu_h_in_0 = acc_h
-        if ah_0 == ah_0_abs_a_in: alu_h_in_0 = abs_a_in
-        if ah_0 == ah_0_neg_a_in: alu_h_in_0 = neg_a_in
-
-        alu_h_in_1 = 0
-        if ah_1 == ah_1_shf:      alu_h_in_1 = shf_h
-        if ah_1 == ah_1_abs_b_in: alu_h_in_1 = abs_b_in
-        if ah_1 == ah_1_neg_b_in: alu_h_in_1 = neg_b_in
-
-        alu_l_in_0 = shf_l
-
-        alu_l_in_1 = 0
-        if al_1 == al_1_acc:      alu_l_in_1 = acc_l
-
-        #print "%08x %016x %08x %08x %d"%(stage1_1, shf_hl, alu_l_in_0, alu_l_in_1, mult4)
-
-        alu_l = alu_l_in_0 + alu_l_in_1
-        alu_l_carry = (alu_l >> 32) & 1
-        alu_l = alu_l & mask32
-
-        alu_h = alu_h_in_0 + alu_h_in_1
-        if carry_chain: alu_h = alu_h + alu_l_carry
-        alu_h_carry = (alu_h >> 32) & 1
-        alu_h = alu_h & mask32
-
-        next_acc_l = alu_l
-        next_acc_h = alu_h
-        if set_bit: next_acc_h = alu_h | (1<<bit_to_set)
-
-        next_a_reg = self.a_reg
-        if ar == a_reg_a_in:     next_a_reg = a_in
-        if ar == a_reg_abs_a_in: next_a_reg = abs_a_in
-        if ar == a_reg_b_shf:    next_a_reg = b_reg << shfb
-
-        next_b_reg = self.b_reg
-        if br == b_reg_b_in:     next_b_reg = b_in
-        if br == b_reg_abs_b_in: next_b_reg = abs_b_in
-
-        #print "%08x %08x"%(next_a_reg, next_b_reg)
-        return (next_acc_l, next_acc_h, next_a_reg, next_b_reg, alu_l_carry)
-
-    #f mul_init_new
-    def mul_init_new(self, a, b, sa=False, sb=False):
-        ah_0=ah_0_zero
-        ah_1=ah_1_zero
-        if sa and (a & sign_bit): ah_1 = ah_1_neg_b_in
-        if sb and (b & sign_bit): ah_0 = ah_0_neg_a_in
-        results = self.comb_new(a_in=a, b_in=b, mult4=0, shf=0, ah_0=ah_0, ah_1=ah_1, al_1=al_1_zero, set_bit=False, bit_to_set=0, carry_chain=False, ar=a_reg_a_in, br=b_reg_b_in )
-        (next_acc_l, next_acc_h, next_a_reg, next_b_reg, alu_l_carry) = results
-        self.accumulator = next_acc_l | (next_acc_h<<32)
-        self.a_reg = next_a_reg
-        self.b_reg = next_b_reg
-        self.stage = 0
-        pass
-
-    #f mul_step_new
-    def mul_step_new(self):
-        results = self.comb_new(a_in=0, b_in=0, mult4=self.b_reg&0xf, shf=self.stage, ah_0=ah_0_acc, ah_1=ah_1_shf, al_1=al_1_acc, set_bit=False, bit_to_set=0, carry_chain=True, ar=a_reg_hold, br=b_reg_hold )
-        (next_acc_l, next_acc_h, next_a_reg, next_b_reg, alu_l_carry) = results
-        self.accumulator = next_acc_l | (next_acc_h<<32)
-        self.a_reg = next_a_reg
-        self.b_reg = self.b_reg >> 4 # next_b_reg
-        self.stage = self.stage + 1
-        return self.b_reg==0
-
-    #f div_init_new
-    def div_init_new(self, a, b, signed=False):
-        if signed:
-            results = self.comb_new(a_in=a, b_in=b, mult4=0, shf=0, ah_0=ah_0_zero, ah_1=ah_1_zero, al_1=al_1_zero, set_bit=False, bit_to_set=0, carry_chain=False, ar=a_reg_abs_a_in, br=b_reg_abs_b_in )
-        else:
-            results = self.comb_new(a_in=a, b_in=b, mult4=0, shf=0, ah_0=ah_0_zero, ah_1=ah_1_zero, al_1=al_1_zero, set_bit=False, bit_to_set=0, carry_chain=False, ar=a_reg_a_in, br=b_reg_b_in )
-        (next_acc_l, next_acc_h, next_a_reg, next_b_reg, alu_l_carry) = results
-        self.accumulator = next_acc_l | (next_acc_h<<32)
-        self.a_reg = next_a_reg
-        self.b_reg = next_b_reg
-        self.stage = 0
-        self.a_shift = 0
-        self.b_shift = 0
-        for i in range(32):
-            if self.a_reg&(1<<i): self.a_shift = 31-i
-            if self.b_reg&(1<<i): self.b_shift = 31-i
-            pass
-        self.b_shift = self.b_shift - self.a_shift
-        self.a_shift = 0
-        print "%08x %08x %016x %d %d"%(self.a_reg, self.b_reg, self.accumulator, self.a_shift, self.b_shift)
-        pass
-
-    #f div_shift
-    def div_shift(self):
-        results = self.comb_new(a_in=0, b_in=0, mult4=1<<(self.a_shift&3), shf=self.a_shift>>2, ah_0=ah_0_zero, ah_1=ah_1_zero, al_1=al_1_zero, set_bit=False, bit_to_set=0, carry_chain=False, ar=a_reg_abs_a_in, br=b_reg_abs_b_in )
-        (next_acc_l, next_acc_h, next_a_reg, next_b_reg, alu_l_carry) = results
-        self.accumulator = next_acc_l | (next_acc_h<<32)
-        #self.a_reg = next_a_reg
-        #self.b_reg = next_b_reg
-        if self.b_shift>=0:
-            self.a_reg = (-(self.b_reg << self.b_shift)) & mask32
-        print "%08x %08x %016x"%(self.a_reg, self.b_reg, self.accumulator)
-        self.stage = self.b_shift - self.a_shift
-        return self.stage<0
-
-    #f div_step_new
-    def div_step_new(self):
-        results = self.comb_new(a_in=0, b_in=0, mult4=1, shf=0, ah_0=ah_0_acc, ah_1=ah_1_zero, al_1=al_1_acc, set_bit=(self.stage>=0), bit_to_set=self.stage, carry_chain=False, ar=a_reg_abs_a_in, br=b_reg_abs_b_in )
-        (next_acc_l, next_acc_h, next_a_reg, next_b_reg, alu_l_carry) = results
-        print "%d:%1d:%08x %08x"%(self.stage,alu_l_carry, next_acc_l, next_acc_h)
-        if (alu_l_carry==1) or (next_acc_l==0):
-            self.accumulator = next_acc_l | (next_acc_h<<32)
-        #self.a_reg = next_a_reg
-        #self.b_reg = next_b_reg
-        self.a_reg = (self.a_reg >> 1) | sign_bit
-        self.stage = self.stage-1
-        print "%08x %08x %016x"%(self.a_reg, self.b_reg, self.accumulator)
-        return (self.stage<0) or (next_acc_l==0)
-
-    #f div_remainder
-    def div_remainder(self, a, b, signed=False):
-        return self.accumulator & mask32
-    #f div_result
-    def div_result(self, a, b, signed=False):
-        return (self.accumulator>>32) & mask32
-
 #c multdiv
 class multdiv:
     """
@@ -254,24 +84,22 @@ class multdiv:
         abs_b_in = b_in
         if (b_in & sign_bit): abs_b_in = neg_b_in
 
-        alu_h_in_0 = 0
-        if ah_0 == ah_0_acc:      alu_h_in_0 = acc_h
-        if ah_0 == ah_0_abs_a_in: alu_h_in_0 = abs_a_in
-        if ah_0 == ah_0_neg_a_in: alu_h_in_0 = neg_a_in
+        alu_h_in_0 = 0 # mul_init
+        if ah_0 == ah_0_acc:      alu_h_in_0 = acc_h    # mul_step, div_step
+        if ah_0 == ah_0_abs_a_in: alu_h_in_0 = abs_a_in # UNUSED
+        if ah_0 == ah_0_neg_a_in: alu_h_in_0 = neg_a_in # mul_init
 
-        alu_h_in_1 = 0
-        if ah_1 == ah_1_shf:      alu_h_in_1 = shf_h
-        if ah_1 == ah_1_abs_b_in: alu_h_in_1 = abs_b_in
-        if ah_1 == ah_1_neg_b_in: alu_h_in_1 = neg_b_in
+        alu_h_in_1 = 0 # mul_init
+        if ah_1 == ah_1_shf:      alu_h_in_1 = shf_h    # mul_step
+        if ah_1 == ah_1_abs_b_in: alu_h_in_1 = abs_b_in # UNUSED
+        if ah_1 == ah_1_neg_b_in: alu_h_in_1 = neg_b_in # mul_init
 
-        alu_l_in_0 = shf_l
+        alu_l_in_0 = shf_l # optional negate?
 
-        alu_l_in_1 = 0
-        if al_1 == al_1_acc:      alu_l_in_1 = acc_l
+        alu_l_in_1 = 0 # mul_init
+        if al_1 == al_1_acc:      alu_l_in_1 = acc_l    # mul_step, div_step
 
-        #print "%08x %016x %08x %08x %d"%(stage1_1, shf_hl, alu_l_in_0, alu_l_in_1, mult4)
-
-        alu_l = alu_l_in_0 + alu_l_in_1
+        alu_l = alu_l_in_0 + alu_l_in_1 # optional carry in?
         alu_l_carry = (alu_l >> 32) & 1
         alu_l = alu_l & mask32
 
@@ -287,7 +115,7 @@ class multdiv:
         next_a_reg = self.a_reg
         if ar == a_reg_a_in:     next_a_reg = a_in
         if ar == a_reg_abs_a_in: next_a_reg = abs_a_in
-        if ar == a_reg_b_shf:    next_a_reg = b_reg << shfb
+        if ar == a_reg_b_shf:    next_a_reg = b_reg << shfb #  UNUSED
 
         next_b_reg = self.b_reg
         if br == b_reg_b_in:     next_b_reg = b_in
@@ -326,8 +154,12 @@ class multdiv:
         Get abs of a and b if signed, else just a and b
         Determine shift
         """
+        self.negate_result    = False
+        self.negate_remainder = False
         if signed:
             results = self.comb_new(a_in=a, b_in=b, mult4=0, shf=0, ah_0=ah_0_zero, ah_1=ah_1_zero, al_1=al_1_zero, set_bit=False, bit_to_set=0, carry_chain=False, ar=a_reg_abs_a_in, br=b_reg_abs_b_in )
+            self.negate_remainder = ((a & sign_bit)!=0)
+            self.negate_result    = (((a ^ b) & sign_bit)!=0)
         else:
             results = self.comb_new(a_in=a, b_in=b, mult4=0, shf=0, ah_0=ah_0_zero, ah_1=ah_1_zero, al_1=al_1_zero, set_bit=False, bit_to_set=0, carry_chain=False, ar=a_reg_a_in, br=b_reg_b_in )
         (next_acc_l, next_acc_h, next_a_reg, next_b_reg, alu_l_carry) = results
@@ -343,7 +175,7 @@ class multdiv:
             pass
         self.b_shift = self.b_shift - self.a_shift
         self.a_shift = 0
-        print "%08x %08x %016x %d %d"%(self.a_reg, self.b_reg, self.accumulator, self.a_shift, self.b_shift)
+        #print "%08x %08x %016x %d %d"%(self.a_reg, self.b_reg, self.accumulator, self.a_shift, self.b_shift)
         pass
 
     #f div_shift
@@ -351,10 +183,9 @@ class multdiv:
         results = self.comb_new(a_in=0, b_in=0, mult4=1<<(self.b_shift&3), shf=self.b_shift>>2, ah_0=ah_0_zero, ah_1=ah_1_zero, al_1=al_1_zero, set_bit=False, bit_to_set=0, carry_chain=False, ar=a_reg_abs_a_in, br=b_reg_abs_b_in )
         (next_acc_l, next_acc_h, next_a_reg, next_b_reg, alu_l_carry) = results
         self.accumulator = next_acc_l | (next_acc_h<<32)
-        #self.b_reg = (-next_acc_l) & mask32
         self.b_reg = (-self.b_reg) & mask32
         self.accumulator = self.a_reg
-        print "%08x %08x %016x"%(self.a_reg, self.b_reg, self.accumulator)
+        #print "%08x %08x %016x"%(self.a_reg, self.b_reg, self.accumulator)
         self.stage = self.b_shift
         return self.stage<0
 
@@ -362,22 +193,23 @@ class multdiv:
     def div_step_new(self):
         results = self.comb_new(a_in=0, b_in=0, mult4=1<<(self.stage&3), shf=(self.stage>>2), ah_0=ah_0_acc, ah_1=ah_1_zero, al_1=al_1_acc, set_bit=(self.stage>=0), bit_to_set=self.stage, carry_chain=False, ar=a_reg_abs_a_in, br=b_reg_abs_b_in )
         (next_acc_l, next_acc_h, next_a_reg, next_b_reg, alu_l_carry) = results
-        print "%d:%1d:%08x %08x"%(self.stage,alu_l_carry, next_acc_l, next_acc_h)
+        #print "%d:%1d:%08x %08x"%(self.stage,alu_l_carry, next_acc_l, next_acc_h)
         if (alu_l_carry==1) or (next_acc_l==0):
             self.accumulator = next_acc_l | (next_acc_h<<32)
-        #self.a_reg = next_a_reg
-        #self.b_reg = next_b_reg
-        #self.b_reg = (self.b_reg >> 1) | sign_bit
         self.stage = self.stage-1
-        print "%08x %08x %016x"%(self.a_reg, self.b_reg, self.accumulator)
+        #print "%08x %08x %016x"%(self.a_reg, self.b_reg, self.accumulator)
         return (self.stage<0) or (next_acc_l==0)
 
     #f div_remainder
     def div_remainder(self, a, b, signed=False):
-        return self.accumulator & mask32
+        remainder = self.accumulator & mask32
+        if self.negate_remainder: return (-remainder) & mask32
+        return remainder
     #f div_result
     def div_result(self, a, b, signed=False):
-        return (self.accumulator>>32) & mask32
+        result = (self.accumulator>>32) & mask32
+        if self.negate_result: return (-result) & mask32
+        return result
 
 #a Toplevel
 def test_mult(test_a,test_b):
@@ -405,8 +237,8 @@ def test_mult(test_a,test_b):
 def test_div(test_a,test_b):
     test_a = test_a & mask32
     test_b = test_b & mask32
-    for signed in [False]: #,True]:
-        for (na,nb) in [(1,1)]:#, (-1,1), (1,-1), (-1,-1)]:
+    for signed in [False, True]:
+        for (na,nb) in [(1,1), (-1,1), (1,-1), (-1,-1)]:
             a = (na * test_a) & mask32
             b = (nb * test_b) & mask32
             if (a & sign_bit): a = a - (1<<32)
@@ -422,10 +254,14 @@ def test_div(test_a,test_b):
             r = x.div_result(a,b,signed)
             rem = x.div_remainder(a,b,signed)
             m    = (a&mask32) / (b&mask32) # unsigned
-            mrem = (a&mask32) % (b&mask32) # unsigned
+            if signed:
+                m    = (a/b) & mask32
+                if (m & sign_bit): m = (m+1) & mask32
+            mrem = (a - (b * m)) & mask32
             err = ""
-            if (r!=m) : err = "DIFFERENT"
-            print("%08x/%08x : %13d / %13d : %13d %13d: %08x %08x  (%08x %08x) %s" % ((a&mask32),(b&mask32),a,b,r,rem,r,rem,m,mrem,err))
+            if (r!=m) :  err += "DIFF RES"
+            if (rem!=mrem) : err = "DIFF REM"
+            print("%d %08x/%08x : %13d / %13d : %13d %13d: %08x %08x  (%08x %08x) %s" % (signed,(a&mask32),(b&mask32),a,b,r,rem,r,rem,m,mrem,err))
 
 if True:
     test_mult(4,5)
